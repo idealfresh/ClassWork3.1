67ec6a67410dd2d60de8ff36f47617e9c9b0163f
owner90 <owner90@192.168.5.3>
2016-10-06 12:36:52 +0300

    Събиране на 2 числа, резултата го умножава по трето число.

---
+++ src/main.java
@@ -0,0 +1,14 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a=2;
+		byte b=2;
+				byte c=2;
+	System.out.println((a+b)*c);
+				
+
+	}
+
+}
